829352dc7c3e972339ef4a168a9a36f6b4ec2371
cstegall07 <charmainestegall@Charmaines-Air.attlocal.net>
2022-02-22 17:19:31 -0500

    updating site

---
+++ projectphase2/bookstoresite/README.md
@@ -0,0 +1,47 @@
+### **Stage 1: Empathise**
+Based on what was discussed in the interview I have created profiles for each employee:
+**Owner**
+
+1. Responsibilities: Oversee inventory at multiple locations, review inventory reports, and the ability to do complete his employee responsibilities
+2. Current system frustrations: Must compile all lists on paper to track inventory
+3. Task timeline: Quarterly basis or as need be
+4. Changing the system would improve efficiency leading to satisfaction.
+
+**Manager**
+
+1. Responsibilities: Enter new books into the system, delete books due to error or at their discretion, track sub-employees usage of the site, and add new employees to the system 
+2. Current system frustrations: Has to edit inventory on paper and keep track of paperwork
+3. Task timeline: Daily
+4. Changing the system would decrease the time it takes to update and delete inventory which would make the Manager happy.
+
+**Staff**
+
+1. Responsibilities: Any available staff member can add books to the system and update information if need be
+2. Current system frustrations: Every book that is added to inventory must be written down
+3. Task timeline: Daily
+4. Changing the system would mean faster processing times leading to staff happiness
+
+### Stage 2: Define
+
+1. Create an inventory system that allows the owner to track inventory at multiple locations
+2. Adapt a login interface that allows all users(owner, manager, and staff) to utilize the site with unique usernames and passwords. 
+3. Create an automated system that marks items as sold by staff as quantity decreases until sold out while remaining in the system unless deleted by managers.
+4. Define search parameters that can be used by managers and staff to look up books and pricing.
+5. Adapt security features such as locking when idle for when managers and staff walk away from the screen.
+6. Create a form that allows managers and staff to add information about books to the site.
+7. Create an admin login for the owner to utilize and assign usernames to managers.
+8. Create a system that allows managers to create and delete  usernames for staff 
+9. Create a specific page with only access granted to managers to see how staff uses the website
+
+### **Stage 3: Ideate**
+ For further details refer to Phase.1.docx in the Project Phase 1 folder
+
+1. Intial Login Page
+2. Secondary Login Page
+3. Intial Login Page Op2
+4. Inventory Deletion
+5. Inventory Addition
+6. Search
+
+### **Stage 4: Prototyping**
+Refer to prototype files found in Project phase 1 folder
---
+++ projectphase2/phase2-problems/scripts.js
@@ -0,0 +1,223 @@
+//scripts for problems
+function resetFindInGrid() {
+    //set tbNumberToFind to blank
+    let tbNumberToFind = document.getElementById("tbNumberToFind");
+    tbNumberToFind.value = '';
+
+    //unhighlight spans
+    let spans = document.getElementsByTagName('span');
+    console.log("spans: ", spans);
+    for (let index = 0; index < spans.length; index++) {
+        spans[index].classList.remove('spanHighlighted');
+    }
+}
+
+function resetFindInList() {
+     //set tbTextToFind to blank
+    let tbTextToFind = document.getElementById("tbTextToFind");
+    tbTextToFind.value = '';
+
+    //unhighlight spans
+    let spans = document.getElementsByTagName('span');
+    console.log("spans: ", spans);
+    for (let index = 0; index < spans.length; index++) {
+        spans[index].classList.remove('spanHighlighted');
+    }
+}
+
+
+
+function reloadPage() {
+    window.location.reload();
+    return false;
+}
+
+
+function findInGrid() {
+ let span = document.getElementsByTagName('span');
+ console.log(span);
+
+ let tbNumberToFind = document.getElementById('tbNumberToFind').value;
+ console.log(tbNumberToFind);
+
+ for(let i = 0; i < span.length; i++){
+ let spanText = span[i].innerText;
+ console.log(spanText);
+
+ if(spanText === tbNumberToFind && tbNumberToFind == 1){
+    console.log('Found 1 in grid!');
+    document.getElementById('span1-1').classList.add('spanHighlighted');
+} else if (spanText === tbNumberToFind && tbNumberToFind == 4){
+    console.log('Found 4 in grid!');
+    document.getElementById('span1-2').classList.add('spanHighlighted');
+}  else if (spanText === tbNumberToFind && tbNumberToFind == 9){
+    console.log('Found 9 in grid!');
+    document.getElementById('span1-3').classList.add('spanHighlighted');
+}  else if (spanText === tbNumberToFind && tbNumberToFind == 8){
+    console.log('Found 8 in grid!');
+    document.getElementById('span2-1').classList.add('spanHighlighted');
+}  else if (spanText === tbNumberToFind && tbNumberToFind == 2){
+    console.log('Found 2 in grid!');
+    document.getElementById('span2-2').classList.add('spanHighlighted');
+}  else if (spanText === tbNumberToFind && tbNumberToFind == 5){
+    console.log('Found 5 in grid!');
+    document.getElementById('span2-3').classList.add('spanHighlighted');
+}  else if (spanText === tbNumberToFind && tbNumberToFind == 6){
+    console.log('Found 6 in grid!');
+    document.getElementById('span3-1').classList.add('spanHighlighted');
+}  else if (spanText === tbNumberToFind && tbNumberToFind == 3){
+    console.log('Found 3 in grid!');
+    document.getElementById('span3-3').classList.add('spanHighlighted');
+} 
+
+let results = document.getElementById('results')
+results.innerHTML = '';
+try{
+    if(tbNumberToFind == "") throw "empty";
+    if(tbNumberToFind < 1) throw "invalid.";
+    if(tbNumberToFind == 7) throw "not in grid.";
+    if(tbNumberToFind > 9) throw "too large for grid.";
+}
+
+catch(err){
+    results.innerHTML = "Input is " + err;
+}
+
+}
+}
+
+function findInList() {
+    let span = document.getElementsByTagName('span');
+    console.log(span);
+
+    let tbTextToFind = document.getElementById('tbTextToFind').value;
+    console.log(tbTextToFind);
+
+    for(let i = 0; i < span.length; i++){
+        let spanText = span[i].innerText;
+        console.log(spanText);
+
+        if( spanText == tbTextToFind && tbTextToFind == 1){
+            console.log('Found 1 in list!');
+            document.getElementById('span1-1').classList.add('spanHighlighted');
+        } else if (spanText === tbTextToFind && tbTextToFind == 4){
+            console.log('Found 4 in list!');
+            document.getElementById('span1-2').classList.add('spanHighlighted');
+        }  else if (spanText === tbTextToFind && tbTextToFind == 9){
+            console.log('Found 9 in list!');
+            document.getElementById('span1-3').classList.add('spanHighlighted');
+        }  else if (spanText === tbTextToFind && tbTextToFind == 8){
+            console.log('Found 8 in list!');
+            document.getElementById('span2-1').classList.add('spanHighlighted');
+        }  else if (spanText === tbTextToFind && tbTextToFind == 2){
+            console.log('Found 2 in list!');
+            document.getElementById('span2-2').classList.add('spanHighlighted');
+        }  else if (spanText === tbTextToFind && tbTextToFind == 5){
+            console.log('Found 5 in list!');
+            document.getElementById('span2-3').classList.add('spanHighlighted');
+        }  else if (spanText === tbTextToFind && tbTextToFind == 6){
+            console.log('Found 6 in list!');
+            document.getElementById('span3-1').classList.add('spanHighlighted');
+        }  else if (spanText === tbTextToFind && tbTextToFind == 'A'){
+            console.log('Found A in list!');
+            document.getElementById('span3-2').classList.add('spanHighlighted');
+        } else if (spanText === tbTextToFind && tbTextToFind == 3){
+            console.log('Found 3 in list!');
+            document.getElementById('span3-3').classList.add('spanHighlighted');
+        } 
+    }
+    let results = document.getElementById('results')
+    results.innerHTML = '';
+    try{
+        if(tbTextToFind == "") throw "empty";
+        if(tbTextToFind < 1 ) throw "invalid.";
+        if(tbTextToFind == 7) throw "not in list.";
+        if(tbTextToFind > 9) throw "too large for list.";
+        //if(tbTextToFind != 'A'  && tbTextToFind == isNaN()) throw "a letter(s) not in list."
+
+    }
+    
+    catch(err){
+        results.innerHTML = "Input is " + err;
+    }  
+  
+}
+
+function sortIt() {
+   var row0, switching, div, shouldSwitch;
+   row0 = document.getElementById("row0");
+   div = row0.getElementsByTagName("div");
+   switching = true;
+
+   while (switching){
+       switching = false;
+       for (i =0; i< (div.length -1); i++) {
+           shouldSwitch = false;
+         if (div[i].innerText > div[i + 1].innerText) {
+             shouldSwitch = true;
+             break;
+         }
+       }
+       if (shouldSwitch) {
+           div[i].parentNode.insertBefore(div[i + 1], div[i]);
+           switching = true;
+       }
+   }
+}
+
+// if it is less than 1 and in a red box place before other red... same for blue and yellow
+
+    
+// while (switching){
+//     switching = false;
+//    span = row0.getElementsByTagName("span");
+//     console.log (span)
+//     for (i =0; i< (span.length -1); i++) {
+//         shouldSwitch = false;
+//       if (span[i].innerHTML > span[i + 1].innerHTML) {
+//           shouldSwitch = true;
+//           break;
+//       }
+//     }
+//     if (shouldSwitch) {
+//         span[i].parentNode.insertBefore(span[i + 1], span[i]);
+//         switching = true;
+//     }
+// }
+
+
+
+   /*let spanColor = [document.getElementsByClassName("column center blue"), document.getElementsByClassName("column center red"), document.getElementsByClassName("column center yellow")];
+    console.log(spanColor);
+
+    let spanRed = document.getElementsByClassName('column center red')
+    console.log(spanRed)
+    let spanBlue = document.getElementsByClassName('column center blue')
+    console.log(spanBlue)
+    let spanYellow = document.getElementsByClassName('column center yellow')
+    console.log(spanYellow)*/
+
+  
+
+   /*      let span = document.getElementsByTagName('span');
+        console.log(span);
+    let spanArr = [];
+    for(let i = 0; i < span.length; i++){
+        let spanText = (span[i].innerText);
+        spanArr.push(spanText);
+        console.log(spanArr)
+        spanArr.sort();
+        console.log(spanArr);
+    }*/
+
+
+
+/*let arr = [4,6,9];
+console.log(arr)
+}
+
+let spanText = Array.from(span[i].innerText);
+let spanTextArr = [];
+spanTextArr.push(...spanText);
+console.log(spanText);
+console.log(spanTextArr);*/
